b4b07884b3ce877ec5f17f31e976992a9374294c
zoff99 <zoff99@users.noreply.github.com>
2023-10-28 11:14:20 +0200

    build jni lib

---
+++ ci_scripts/deps_macos.sh
@@ -0,0 +1,123 @@
+#! /bin/bash
+
+_SCRIPTDIR_=$(dirname $0)
+export _SCRIPTDIR_
+_SCRIPTDIR2_=$(cd $_SCRIPTDIR_;pwd)
+export _SCRIPTDIR2_
+
+_HOME_="$(pwd)"
+export _HOME_
+
+cd "$_HOME_"
+
+export _SRC_=$_HOME_/src/
+export _INST_=$_HOME_/inst/
+
+mkdir -p $_SRC_
+mkdir -p $_INST_
+
+export LD_LIBRARY_PATH=$_INST_/lib/
+export PKG_CONFIG_PATH=$_INST_/lib/pkgconfig
+
+
+# ----------- config ------------
+ORIGPATH=$PATH
+export ORIGPATH
+NEWPATH=$PATH # /usr/x86_64-w64-mingw32/bin:$PATH
+export NEWPATH
+export PATH=$NEWPATH
+
+# MAKEFLAGS=j$(nproc)
+# export MAKEFLAGS
+
+WGET_OPTIONS="--timeout=10"
+export WGET_OPTIONS
+
+export CXXFLAGS="$CXXFLAGS -fPIC"
+export CFLAGS="$CFLAGS -fPIC"
+# ----------- config ------------
+
+type sudo
+
+# ------- deps verisions ---------
+FFMPEG_VERSION="n6.0"
+# ------- deps verisions ---------
+
+# ---------- ffmpeg ---------
+if [ 1 == 1 ]; then
+
+cd "$_SRC_"
+
+FFMPEG_FILENAME="$FFMPEG_VERSION.tar.gz"
+rm -f "ffmpeg"*.tar.*
+wget $WGET_OPTIONS "https://github.com/FFmpeg/FFmpeg/archive/refs/tags/$FFMPEG_FILENAME" -O "ffmpeg_""$FFMPEG_FILENAME"
+tar -xf "ffmpeg_""$FFMPEG_FILENAME"
+rm -f "ffmpeg"*.tar.*
+cd *mpeg*/
+
+export LDFLAGS=" "
+
+#
+# sadly need "--disable-asm" or you will get:
+#
+# libavcodec.a(h264_qpel_10bit.o): relocation R_X86_64_PC32 against symbol `ff_pw_1023' can not be used when making a shared object; recompile with -fPIC
+#
+
+  ./configure  \
+              --enable-gpl \
+              --prefix="$_INST_" \
+              --disable-asm \
+              --enable-pic \
+              --enable-swscale \
+              --disable-network \
+              --disable-everything \
+              --disable-debug \
+              --disable-shared \
+              --disable-programs \
+              --disable-protocols \
+              --disable-doc \
+              --disable-sdl2 \
+              --disable-avfilter \
+              --disable-filters \
+              --disable-iconv \
+              --disable-network \
+              --disable-postproc \
+              --disable-swscale-alpha \
+              --disable-dct \
+              --disable-dwt \
+              --disable-lsp \
+              --disable-mdct \
+              --disable-rdft \
+              --disable-fft \
+              --disable-faan \
+              --disable-vaapi \
+              --disable-vdpau \
+              --disable-zlib \
+              --disable-bzlib \
+              --disable-lzma \
+              --disable-encoders \
+              --disable-decoders \
+              --disable-demuxers \
+              --disable-parsers \
+              --disable-bsfs \
+              --enable-swresample \
+              --enable-avcodec \
+              --enable-indev=avfoundation \
+              --enable-demuxer=rawvideo \
+              --enable-decoder=rawvideo \
+              --enable-demuxer=pcm_s16le \
+              --enable-decoder=pcm_s16le && \
+
+  make -j || exit 1
+  make install
+
+cd "$_HOME_"
+
+ls -al "$_INST_"/lib/
+
+fi
+# ---------- ffmpeg ---------
+
+
+
+
---
+++ ci_scripts/java_jni_lib_macos.sh
@@ -0,0 +1,117 @@
+#! /bin/bash
+
+_HOME_="$(pwd)"
+export _HOME_
+
+_HOME2_=$(dirname $0)
+export _HOME2_
+_HOME3_=$(cd $_HOME2_;pwd)
+export _HOME3_
+
+id -a
+pwd
+ls -al
+
+export _SRC_=$_HOME_/src/
+export _INST_=$_HOME_/inst/
+
+
+mkdir -p $_SRC_
+mkdir -p $_INST_
+
+export LD_LIBRARY_PATH=$_INST_/lib/
+export PKG_CONFIG_PATH=$_INST_/lib/pkgconfig
+
+
+# ----------- config ------------
+ORIGPATH=$PATH
+export ORIGPATH
+NEWPATH=$PATH # /usr/x86_64-w64-mingw32/bin:$PATH
+export NEWPATH
+export PATH=$NEWPATH
+
+# MAKEFLAGS=j$(nproc)
+# export MAKEFLAGS
+
+WGET_OPTIONS="--timeout=10"
+export WGET_OPTIONS
+
+# ----------- config ------------
+
+
+## ---------------------------
+pwd
+ls -al
+## ---------------------------
+
+echo "JAVADIR1------------------"
+find /usr -name 'jni.h'
+echo "JAVADIR1------------------"
+
+# /usr/local/Cellar/openjdk/15.0.2/include/jni.h
+# /usr/local/Cellar/openjdk/15.0.2/include/jni_md.h
+
+# /usr/local/Cellar/openjdk/15.0.2/libexec/openjdk.jdk/Contents/Home/include/jni.h
+# /usr/local/Cellar/openjdk/15.0.2/libexec/openjdk.jdk/Contents/Home/include/darwin/jni_md.h
+
+echo "JAVADIR2------------------"
+find /usr -name 'jni_md.h'
+echo "JAVADIR2------------------"
+
+#dirname $(find /usr -name 'jni.h' 2>/dev/null|grep -v 'libavcodec'|head -1) > /tmp/xx1
+#dirname $(find /usr -name 'jni_md.h' 2>/dev/null|head -1) > /tmp/xx2
+#export JAVADIR1=$(cat /tmp/xx1)
+#export JAVADIR2=$(cat /tmp/xx2)
+
+#export JAVADIR1="/usr/local/Cellar/openjdk/16.0.1/include"
+#export JAVADIR2="/usr/local/Cellar/openjdk/16.0.1/include"
+
+#if [ ! -e "$JAVADIR1" ]; then
+#    mkdir -p "$_INST_/jinclude/"
+#    cp -av /Users/travis/build/zoff99/java_toxclient_example/circle_scripts/jni_md.h "$_INST_/jinclude/"
+#    cp -av /Users/travis/build/zoff99/java_toxclient_example/circle_scripts/jni.h "$_INST_/jinclude/"
+#    export JAVADIR1="$_INST_/jinclude"
+#    export JAVADIR2="$_INST_/jinclude"
+#fi
+
+#echo "JAVADIR1:""$JAVADIR1"
+#echo "JAVADIR2:""$JAVADIR2"
+
+mkdir -p "$_INST_/jinclude/"
+cp -av "$_HOME3_"/jni_md.h "$_INST_/jinclude/"
+cp -av "$_HOME3_"/jni.h "$_INST_/jinclude/"
+
+export CFLAGS=" -fPIC -g -O3 -std=gnu99 -I$_INST_/include/ -I$_INST_/jinclude/ -L$_INST_/lib -fstack-protector-all "
+
+gcc $CFLAGS \
+-Wall \
+-Wno-unused-function \
+-Wno-discarded-qualifiers \
+-Wno-unused-const-variable \
+-Wno-deprecated-declarations \
+-D_FILE_OFFSET_BITS=64 -D__USE_GNU=1 \
+-I$JAVADIR1/ \
+-I$JAVADIR2/ \
+ffmpeg_av_jni.c \
+$_INST_/lib/libavcodec.a \
+$_INST_/lib/libavdevice.a \
+$_INST_/lib/libavformat.a \
+$_INST_/lib/libavutil.a \
+$_INST_/lib/libswscale.a \
+$_INST_/lib/libswresample.a \
+-lpthread \
+-lm \
+-shared \
+-o libffmpeg_av_jni.jnilib || exit 1
+
+
+ls -al libffmpeg_av_jni.jnilib || exit 1
+
+otool -L libffmpeg_av_jni.jnilib
+pwd
+file libffmpeg_av_jni.jnilib
+
+pwd
+find . -name libffmpeg_av_jni.jnilib
+
+
