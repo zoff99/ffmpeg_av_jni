ee757a5d582fd29881fae35e7c212f379d3e88ef
zoff99 <zoff99@users.noreply.github.com>
2023-11-01 09:08:21 +0100

    update version

--- ffmpeg_av_jni.c
+++ ffmpeg_av_jni.c
@@ -49,8 +49,8 @@
 // ----------- version -----------
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 99
-#define VERSION_PATCH 1
-static const char global_version_string[] = "0.99.1";
+#define VERSION_PATCH 2
+static const char global_version_string[] = "0.99.2";
 // ----------- version -----------
 // ----------- version -----------
 
