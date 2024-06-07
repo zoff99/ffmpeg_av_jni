2fe28ec2e9f38bc9cbc0dfb771f4ff6349ef2ba6
zoff99 <zoff99@users.noreply.github.com>
2024-06-07 18:32:53 +0200

    v0.99.25

--- ffmpeg_av_jni.c
+++ ffmpeg_av_jni.c
@@ -55,9 +55,9 @@
 // ----------- version -----------
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 99
-#define VERSION_PATCH 24
-static const char global_version_string[] = "0.99.24";
-static const char global_version_asan_string[] = "0.99.24-ASAN";
+#define VERSION_PATCH 25
+static const char global_version_string[] = "0.99.25";
+static const char global_version_asan_string[] = "0.99.25-ASAN";
 // ----------- version -----------
 // ----------- version -----------
 
