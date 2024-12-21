21b9130d8ceaea644cfe884a317aefd5fa5578cc
zoff99 <zoff99@users.noreply.github.com>
2024-12-21 23:12:19 +0100

    x

--- avfoundation_list_devices.m
+++ avfoundation_list_devices.m
@@ -2,26 +2,26 @@
 #import <Foundation/Foundation.h>
 
 static NSArray* getDevicesWithMediaType(AVMediaType mediaType) {
-#if ((TARGET_OS_OSX && __MAC_OS_X_VERSION_MIN_REQUIRED >= 101500))
+#if (__MAC_OS_X_VERSION_MIN_REQUIRED >= 101500)
     NSMutableArray *deviceTypes = nil;
     if (mediaType == AVMediaTypeVideo) {
         deviceTypes = [NSMutableArray arrayWithArray:@[AVCaptureDeviceTypeBuiltInWideAngleCamera]];
-        #if (TARGET_OS_OSX && __MAC_OS_X_VERSION_MIN_REQUIRED >= 130000)
+        #if (__MAC_OS_X_VERSION_MIN_REQUIRED >= 130000)
             [deviceTypes addObject: AVCaptureDeviceTypeDeskViewCamera];
         #endif
-        #if ((TARGET_OS_OSX && __MAC_OS_X_VERSION_MIN_REQUIRED >= 140000))
+        #if (__MAC_OS_X_VERSION_MIN_REQUIRED >= 140000)
             [deviceTypes addObject: AVCaptureDeviceTypeContinuityCamera];
         #endif
     } else if (mediaType == AVMediaTypeAudio) {
-        #if ((TARGET_OS_OSX && __MAC_OS_X_VERSION_MIN_REQUIRED >= 140000))
+        #if (__MAC_OS_X_VERSION_MIN_REQUIRED >= 140000)
             deviceTypes = [NSMutableArray arrayWithArray:@[AVCaptureDeviceTypeMicrophone]];
         #else
             deviceTypes = [NSMutableArray arrayWithArray:@[AVCaptureDeviceTypeBuiltInMicrophone]];
         #endif
     } else if (mediaType == AVMediaTypeMuxed) {
-        #if ((TARGET_OS_OSX && __MAC_OS_X_VERSION_MIN_REQUIRED >= 140000))
+        #if (__MAC_OS_X_VERSION_MIN_REQUIRED >= 140000)
             deviceTypes = [NSMutableArray arrayWithArray:@[AVCaptureDeviceTypeExternal]];
-        #elif (TARGET_OS_OSX && __MAC_OS_X_VERSION_MIN_REQUIRED < 140000)
+        #elif (__MAC_OS_X_VERSION_MIN_REQUIRED < 140000)
             deviceTypes = [NSMutableArray arrayWithArray:@[AVCaptureDeviceTypeExternalUnknown]];
         #else
             return nil;
@@ -46,8 +46,47 @@ int main(int argc, const char* argv[])
     NSLog(@"lsdev:start");
     NSArray *devices       = getDevicesWithMediaType(AVMediaTypeVideo);
     NSArray *devices_muxed = getDevicesWithMediaType(AVMediaTypeMuxed);
+    int num_video_devices = [devices count] + [devices_muxed count];
     NSLog(@"video: %@", devices);
     NSLog(@"audio: %@", devices_muxed);
+
+
+    uint32_t num_screens = 0;
+#if (__MAC_OS_X_VERSION_MIN_REQUIRED >= 1070)
+    CGGetActiveDisplayList(0, NULL, &num_screens);
+#endif
+
+    int index = 0;
+    NSLog(@"AVFoundation video devices:");
+    for (AVCaptureDevice *device in devices) {
+        const char *name = [[device localizedName] UTF8String];
+        index            = [devices indexOfObject:device];
+        NSLog(@"[%d] %s", index, name);
+    }
+    for (AVCaptureDevice *device in devices_muxed) {
+        const char *name = [[device localizedName] UTF8String];
+        index            = [devices count] + [devices_muxed indexOfObject:device];
+        NSLog(@"[%d] %s", index, name);
+    }
+#if (__MAC_OS_X_VERSION_MIN_REQUIRED >= 1070)
+    if (num_screens > 0) {
+        CGDirectDisplayID screens[num_screens];
+        CGGetActiveDisplayList(num_screens, screens, &num_screens);
+        for (int i = 0; i < num_screens; i++) {
+            NSLog(@"[%d] Capture screen %d", ctx->num_video_devices + i, i);
+        }
+    }
+#endif
+
+    NSLog(@"AVFoundation audio devices:");
+    devices = getDevicesWithMediaType(AVMediaTypeAudio);
+    for (AVCaptureDevice *device in devices) {
+        const char *name = [[device localizedName] UTF8String];
+        int index  = [devices indexOfObject:device];
+        NSLog(@"[%d] %s", index, name);
+    }
+
+
     return 0;
 }
 
